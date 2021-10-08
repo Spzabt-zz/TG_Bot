cf57c981e561ae02956bc97415ad9ac1a9f0a219
Szpabt-zz <stoida146@gmail.com>
2021-10-08 22:20:32 +0300

    TG_Bot commit

---
+++ TG_Bot/CurrencyPairsParser.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Helpers;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TG_Bot
+{
+    public class CurrencyPairsParser
+    {
+        private readonly WebClient _webClient;
+        private const string Link = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+
+        public CurrencyPairsParser()
+        {
+            _webClient = new WebClient();
+        }
+
+        public string GetUSD_UAHPair(string message)
+        {
+            string response = _webClient.DownloadString(Link);
+            var data = JsonConvert.DeserializeObject<List<JsonData>>(response);
+            foreach (var jsonData in data)
+            {
+                if (message == jsonData.Ccy)
+                {
+                    return jsonData.Ccy + "/" + jsonData.Base_Ccy + " - Buy: " + jsonData.Buy + "\n"
+                           + jsonData.Ccy + "/" + jsonData.Base_Ccy + " - Sell: " + jsonData.Sale;
+                }
+            }
+
+            return "Some error :(";
+        }
+    }
+
+    public class JsonData
+    {
+        public string Ccy { get; set; }
+        public string Base_Ccy { get; set; }
+        public float Buy { get; set; }
+        public float Sale { get; set; }
+    }
+}
---
+++ TG_Bot/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TG_Bot
+{
+    class Program
+    {
+        private static string Token { get; set; } = "1876809111:AAGXSbIRLcwMrTEdFwH2QKM-XPZGViZRKP0";
+        private static ITelegramBotClient _botClient;
+        //private static TelegramBotClient _botClient;
+        private static Random _random;
+        private static CurrencyPairsParser _currencyPairs;
+
+        static void Main(string[] args)
+        {
+            _botClient = new TelegramBotClient(Token);
+            _currencyPairs = new CurrencyPairsParser();
+            _botClient.StartReceiving();
+            _botClient.OnMessage += OnMessageHandler;
+            Console.ReadLine();
+            _botClient.StopReceiving();
+        }
+
+        private static async void OnMessageHandler(object? sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Text == null) return;
+            string valuePairInput = string.Empty;
+            // await _botClient.SendTextMessageAsync(
+            //     msg.Chat.Id,
+            //     msg.Text,
+            //     parseMode: ParseMode.Markdown,
+            //     disableNotification: true,
+            //     replyMarkup: GetButtons());
+            //Console.WriteLine($"Msg: {msg.Text}");
+            Console.WriteLine($"{msg.From.FirstName} sent message {msg.MessageId}" +
+                              $"to chat {msg.Chat.Id} at {msg.Date.ToLocalTime()}. " +
+                              $"It is a reply to message {msg.MessageId} " +
+                              $"and has {msg.Text.Length} message entities.");
+            switch (msg.Text)
+            {
+                case "Send sticker pls!":
+                    /*var sticker =*/
+                    _random = new Random();
+                    var stickers = new[]
+                    {
+                        $"https://cdn.tlgrm.app/stickers/c62/4a8/c624a88d-1fe3-403a-b41a-3cdb9bf05b8a/192/{_random.Next(1, 49)}.webp",
+                        $"https://cdn.tlgrm.app/stickers/c62/4a8/c624a88d-1fe3-403a-b41a-3cdb9bf05b8a/192/{_random.Next(1, 49)}.webp",
+                        $"https://cdn.tlgrm.app/stickers/c62/4a8/c624a88d-1fe3-403a-b41a-3cdb9bf05b8a/192/{_random.Next(1, 49)}.webp"
+                    };
+                    foreach (var sticker in stickers)
+                        await _botClient.SendStickerAsync(
+                            chatId: msg.Chat.Id,
+                            sticker: sticker,
+                            //replyToMessageId: msg.MessageId,
+                            replyMarkup: GetButtons()
+                        );
+
+                    break;
+                // case "Send audio":
+                //     await _botClient.SendAudioAsync(
+                //         chatId: msg.Chat.Id,
+                //         audio: "https://open.spotify.com/track/4ZtFanR9U6ndgddUvNcjcG?si=d1947cfa36134b5e"
+                //     );
+                //     break;
+                case "Send pic":
+                    await _botClient.SendPhotoAsync(
+                        msg.Chat.Id,
+                        photo:
+                        "https://www.komar.de/ru/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/D/1/D1-017_1583159159.jpg",
+                        //replyToMessageId: msg.MessageId,
+                        replyMarkup: GetButtons());
+                    break;
+                // case "Send vid":
+                //     await using (var stream =
+                //         System.IO.File.OpenRead(@"D:\Videos\fnf movies\Skippa 2021-08-28 10-32-22-999.mp4"))
+                //     {
+                //         await _botClient.SendVideoAsync(
+                //             chatId: msg.Chat.Id,
+                //             video: stream,
+                //             replyToMessageId: msg.MessageId,
+                //             replyMarkup: GetButtons());
+                //     }
+                //
+                //     break;
+                case "Currency pairs":
+                    var currencies = new[]
+                    {
+                        "USD", "EUR", "RUR", "BTC"
+                    };
+                    await _botClient.SendTextMessageAsync(
+                        msg.Chat.Id,
+                        text: "From privat24 API");
+                    foreach (var currency in currencies)
+                        await _botClient.SendTextMessageAsync(
+                            msg.Chat.Id,
+                            text: _currencyPairs.GetUSD_UAHPair(currency),
+                            replyMarkup: GetButtons());
+                    break;
+                default:
+                    await _botClient.SendTextMessageAsync(
+                        msg.Chat.Id,
+                        text: "Choose command!",
+                        replyMarkup: GetButtons());
+                    break;
+            }
+            //await _botClient.SendTextMessageAsync(msg.Chat.Id, msg.Text, replyMarkup:GetButtons());
+            // var sticker = await _botClient.SendStickerAsync(
+            //     chatId: msg.Chat.Id,
+            //     sticker:
+            //     "https://cdn.tlgrm.app/stickers/c62/4a8/c624a88d-1fe3-403a-b41a-3cdb9bf05b8a/thumb128.webp",
+            //     replyToMessageId: msg.MessageId);
+        }
+
+        private static IReplyMarkup GetButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>
+                    {
+                        new KeyboardButton {Text = "Send audio"},
+                        new KeyboardButton {Text = "Send voice"},
+                        new KeyboardButton {Text = "Send vid"}
+                    },
+                    new List<KeyboardButton>
+                    {
+                        new KeyboardButton {Text = "Send sticker pls!"},
+                        new KeyboardButton {Text = "Send pic"},
+                        new KeyboardButton {Text = "Currency pairs"}
+                    }
+                },
+                OneTimeKeyboard = true, ResizeKeyboard = true
+            };
+        }
+    }
+}
